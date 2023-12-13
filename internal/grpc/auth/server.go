c0df1db997921867af6a6264bfc4d09e97aa6fa8
Aleksandr.Gaakh <gaach006@mail.ru>
2023-12-14 00:57:52 +0300

    add unimplemented grpc handlers

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -1,9 +1,29 @@
 package auth
 
 import (
+	"context"
 	ssov1 "github.com/Qw1LL/auth_protos/gen/go/sso"
+	"google.golang.org/grpc"
 )
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
 }
+
+func Register(gRpcServer *grpc.Server) {
+	ssov1.RegisterAuthServer(gRpcServer, &serverAPI{})
+}
+
+func (s *serverAPI) Login(ctx context.Context, req *ssov1.LoginRequest) (*ssov1.LoginResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverAPI) Register(ctx context.Context, req *ssov1.RegisterRequest) (*ssov1.RegisterResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverAPI) IsAdmin(ctx context.Context, req *ssov1.IsAdminRequest) (*ssov1.IsAdminResponse, error) {
+	panic("implement me")
+}
+
+func (s *serverAPI) mustEmbedUnimplementedAuthServer() {}
